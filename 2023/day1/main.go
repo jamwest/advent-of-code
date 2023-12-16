016a6d3d6aa2fac56bfcc34b12a5c5a6cfd031ef
jamwest <jptwest@gmail.com>
2023-12-16 17:18:05 +1300

    day1 part one solution

--- 2023/day1/main.go
+++ 2023/day1/main.go
@@ -11,16 +11,41 @@ func check(e error) {
 	}
 }
 
+func partOneLine(line []byte) int {
+	sum := 0
+
+	for i := 0; i < len(line); i++ {
+		if line[i] >= 48 && line[i] <= 58 {
+			sum = int(line[i]-48) * 10
+			break
+		}
+	}
+
+	for i := len(line) - 1; i >= 0; i-- {
+		if line[i] >= 48 && line[i] <= 58 {
+			sum = sum + int(line[i]-48)
+			break
+		}
+	}
+	return sum
+}
+
+func partOne(scanner *bufio.Scanner) int {
+	sum := 0
+
+	for scanner.Scan() {
+		sum = sum + partOneLine(scanner.Bytes())
+	}
+	return sum
+}
+
 func main() {
 	f, err := os.Open("./input.txt")
 	check(err)
+	defer f.Close()
 
-	buf := bufio.NewReader(f)
-
-	line, _, err := buf.ReadLine()
-	check(err)
-
-	println(string(line))
-
-	f.Close()
+	buf := bufio.NewScanner(f)
+	partOne := partOne(buf)
+	check(buf.Err())
+	println(partOne)
 }
